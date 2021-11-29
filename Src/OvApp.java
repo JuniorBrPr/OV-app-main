c4caf396168309417a6854c8393dc48d74f5df45
Niels van Gortel <niels.vangortel@student.hu.nl>
2021-11-29 21:11:16 +0000

    Upload New File

---
+++ Src/OvApp.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+public class OvApp
+{
+
+
+        public static void main(String[] args)
+        {
+            HomeGui g = new HomeGui();
+            g.setBounds(400,200,600,400);
+            g.setVisible(true);
+        }
+}
+
+
